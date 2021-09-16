9bce0f3e11420d4981d0f0ba090a8a9ec88a7f54
Bhushan Mendhe <bhushanmendhe15@gmail.com>
2021-09-17 01:56:20 +0530

    wired fetch call

--- banana.js
+++ banana.js
@@ -2,8 +2,18 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#txt-output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTranslationURL(text) {
+    return serverURL + "?text=" + text;
+}
+
 function clickEventHandler() {
-    txtOutput.innerText = txtInput.value;
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(resp => resp.json())
+    .then(translated => console.log(translated.contents.translated));
 };
 
 btnTranslate.addEventListener("click", clickEventHandler);
