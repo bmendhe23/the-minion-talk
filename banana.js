4c206e983a9296991b7e38f9af63785da5c410a7
Bhushan Mendhe <bhushanmendhe15@gmail.com>
2021-09-17 02:16:26 +0530

    connected to original API

--- banana.js
+++ banana.js
@@ -2,7 +2,11 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#txt-output");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function errorHandler(error) {
+    console.log("error occured: " + error);
+}
 
 function getTranslationURL(text) {
     return serverURL + "?text=" + text;
@@ -13,7 +17,11 @@ function clickEventHandler() {
 
     fetch(getTranslationURL(inputText))
     .then(resp => resp.json())
-    .then(translated => console.log(translated.contents.translated));
+    .then(translated => {
+        txtTranslated = translated.contents.translated;
+        txtOutput.innerText = txtTranslated;
+    })
+    .catch(errorHandler);
 };
 
 btnTranslate.addEventListener("click", clickEventHandler);
